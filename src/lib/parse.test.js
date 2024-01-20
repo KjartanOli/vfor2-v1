656894bf4b24fc60f7351768700d9c97600ca3f5
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-01-20 15:40:15 +0000

    Add parsing functions
    * src/lib/parse.js (parseTeamsJson): Remove.
      (parse_file):
      (parse_teams_file):
      (parse_game_file):
      (parse_dir):
    
      New functions.
    
    * src/lib/parse.test.js: Add tests for the new parsing functions.
    
    * src/test/data/3/gameday-1.json:
    * src/test/data/3/teams.json:
    * src/test/data/4/gameday-1.json:
    * src/test/data/5/gameday-1.json:
    * src/test/data/5/teams.json:
    
      Test data for the new tests.

--- src/lib/parse.test.js
+++ src/lib/parse.test.js
@@ -1,10 +1,51 @@
 import { describe, expect, it } from '@jest/globals';
-import { parseTeamsJson } from './parse';
+import { parse_dir } from './parse';
 
+// The only public function in parse is `parse_dir` which does file
+// system interaction and therefore can not be unit tested.
 describe('parse', () => {
-  describe.only('parseTeamsJson', () => {
-    it('should have a test', () => {
-      expect(parseTeamsJson('{}')).toEqual({});
-    });
-  });
-});
+  describe.only('parse_dir', () => {
+    it('Valid', async () => {
+			const data = await parse_dir('src/test/data/3');
+      expect(data).toEqual({
+				teams: ['lorem', 'ipsum'],
+				gamedays: [
+					{
+						date: Date.parse('2024-02-02T15:20:53.955Z'),
+						games: [
+							{
+								home: {
+									name: 'lorem',
+									score: 0
+								},
+								away: {
+									name: 'ipsum',
+									score: 0
+								}
+							}
+						]
+					}]
+			})
+		});
+
+		it('Missing teams.json', async () => {
+			const data = await parse_dir('src/test/data/4');
+			expect(data).toEqual(null);
+		});
+
+		it('Directory does not exist', async () => {
+			const data = await parse_dir('src/test/data/none');
+			expect(data).toEqual(null);
+		});
+
+		it('Invalid game day', async () => {
+			const data = await parse_dir('src/test/data/5');
+			expect(data).toEqual({
+				teams: ['lorem', 'ipsum'],
+				gamedays: []
+			});
+		});
+	}
+	);
+}
+);
