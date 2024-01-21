535d1d3c80eed0c7b692c32b9899563221c9e0f9
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-01-21 13:04:56 +0000

    Add some HTML generation functions
    * src/lib/html.js (indexTemplate): Remove.
      (el):
      (game_list):
      (team_list):
    
      New functions.
    * src/lib/html.test.js: Add tests for the new functions.

--- src/lib/html.js
+++ src/lib/html.js
@@ -1,3 +1,50 @@
-export function indexTemplate() {
-  return 'html';
-}
+function el(element, attributes, ...children)
+	{
+		const attrs = Object.entries(attributes)
+			.map(entry => {
+				const [key, value] = entry;
+				return `${key}="${value}"`
+			});
+
+		return `<${element}${attrs.length > 0 ? ' ' : ''}${attrs.join(' ')}>${children.join('')}</${element}>`
+	}
+
+export function game_list(gamedays)
+	{
+		return el('table', {},
+			el('thead', {},
+				el('tr', {},
+					el('th', {}, 'Date'),
+					el('th', {}, 'Home'),
+					el('th', {}, 'Away'),
+					el('th', {}, 'Score')
+				)
+			),
+			el('tbody', {},
+				...gamedays.map(day => day.games.map(game => el('tr', {},
+					el('td', {}, day.date.toISOString().substring(0, 'yyyy-mm-dd'.length)),
+					el('td', {}, game.home.name),
+					el('td', {}, game.away.name),
+					el('td', {}, `${game.home.score}-${game.away.score}`)
+				)))
+			)
+		)
+	}
+
+export function team_list(teams)
+	{
+		return el('table', {},
+			el('thead', {},
+				el('tr', {},
+					el('th', {}, 'Team'),
+					el('th', {}, 'Score')
+				)
+			),
+			el('tbody', {},
+				...teams.map(team => el('tr', {id: team.name},
+					el('td', {}, team.name),
+					el('td', {}, team.score)
+				))
+			)
+		);
+	}
--- src/lib/html.test.js
+++ src/lib/html.test.js
@@ -1,10 +1,37 @@
 import { describe, expect, it } from '@jest/globals';
-import { indexTemplate } from './html';
+import { game_list, team_list } from './html';
 
 describe('html', () => {
-  describe.only('indexTemplate', () => {
-    it('should have a test', () => {
-      expect(indexTemplate()).toBe('html');
-    });
-  });
+	describe.only('game_list', () => {
+		it('Single game', () => {
+			expect(game_list([
+				{
+					date: new Date('2024-02-02T15:20:53.955Z'),
+					games: [
+						{
+							home: {
+								name: 'lorem',
+								score: 3,
+							},
+							away: {
+								name: 'ipsum',
+								score: 2,
+							},
+						}
+					]
+				}
+			])).toEqual('<table><thead><tr><th>Date</th><th>Home</th><th>Away</th><th>Score</th></tr></thead><tbody><tr><td>2024-02-02</td><td>lorem</td><td>ipsum</td><td>3-2</td></tr></tbody></table>');
+		})
+	});
+
+	describe.only('team_list', () => {
+		it('One team', () => {
+			expect(team_list([
+				{
+					name: 'lorem',
+					score: 4
+				}
+			])).toEqual('<table><thead><tr><th>Team</th><th>Score</th></tr></thead><tbody><tr id="lorem"><td>lorem</td><td>4</td></tr></tbody></table>');
+		})
+	});
 });
