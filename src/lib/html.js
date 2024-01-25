a87a986d13b839d29e7998267213ff3bf745f8ff
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-01-25 16:34:04 +0000

    Change formatting
    * src/lib/html.js: Change formatting.

--- src/lib/html.js
+++ src/lib/html.js
@@ -1,21 +1,19 @@
-function el(element, attributes, ...children)
-	{
-		const attrs = Object.entries(attributes)
-			.map(entry => {
-				const [key, value] = entry;
-				return `${key}="${value}"`
-			});
+function el(element, attributes, ...children) {
+	const attrs = Object.entries(attributes)
+		.map(entry => {
+			const [key, value] = entry;
+			return `${key}="${value}"`
+		});
 
-		return `<${element}${attrs.length > 0 ? ' ' : ''}${attrs.join(' ')}>${children.join('')}</${element}>`
-	}
+	return `<${element}${attrs.length > 0 ? ' ' : ''}${attrs.join(' ')}>${children.join('')}</${element}>`
+}
 
-export function document(title, language, head, ...body)
-	{
-		return el('html', {lang: language},
-			el('head', {}, ...[el('title', {}, title), ...head]),
-			el('body', {}, ...body)
-		);
-	}
+export function document(title, language, head, ...body) {
+	return el('html', { lang: language },
+		el('head', {}, ...[el('title', {}, title), ...head]),
+		el('body', {}, ...body)
+	);
+}
 
 export function game_list(gamedays) {
 	return el('table', {},
@@ -39,20 +37,19 @@ export function game_list(gamedays) {
 	);
 }
 
-export function team_list(teams)
-	{
-		return el('table', {},
-			el('thead', {},
-				el('tr', {},
-					el('th', {}, 'Team'),
-					el('th', {}, 'Score')
-				)
-			),
-			el('tbody', {},
-				...teams.map(team => el('tr', {id: team.name},
-					el('td', {}, team.name),
-					el('td', {}, team.score)
-				))
+export function team_list(teams) {
+	return el('table', {},
+		el('thead', {},
+			el('tr', {},
+				el('th', {}, 'Team'),
+				el('th', {}, 'Score')
 			)
-		);
-	}
+		),
+		el('tbody', {},
+			...teams.map(team => el('tr', { id: team.name },
+				el('td', {}, team.name),
+				el('td', {}, team.score)
+			))
+		)
+	);
+}
