5924c83836a2cc5b719e1a49332c8307746fd3fd
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-01-25 16:36:58 +0000

    Add some functions to calculate scores
    * src/lib/score.js (game_winner, is_draw, points_for_game,
      played_in_game_p, points, league_status): New functions.
    * src/lib/score.test.js: Add tests.

--- src/lib/score.js
+++ src/lib/score.js
@@ -1,4 +1,85 @@
+/**
+ * Get the winner of a game.
+ * @param {Game} game Game whose winner should be checked
+ * @returns {string | null} Name of the winning team, if there is no
+ * winner `null` is returned.
+ */
+function game_winner(game)
+{
+	const { home, away } = game;
+	if (home.score > away.score)
+		return home.name;
+	if (away.score > home.score)
+		return away.name;
+	return null;
+}
 
-export function calculateStandings() {
-  return 0;
+/**
+ * Check whether a game was a draw
+ * @param {Game} game Game to check
+ * @returns `true` if game ended in a draw, `false` otherwise
+ */
+function is_draw(game)
+{
+	const { home, away } = game;
+	return home.score === away.score;
+}
+
+/**
+ * Get the number of points a team receives for a game.
+ *
+ * team must have played in the game.
+ *
+ * @param {string} team Team whose points should be calculated
+ * @param {Game} game Game for which the points should be calculated
+ * @returns {number} team's points for game.
+ */
+function points_for_game(team, game)
+{
+	if (is_draw(game))
+		return 1;
+	if (team === game_winner(game))
+		return 3;
+
+	return 0;
+}
+
+/**
+ * Check whether a team played in a game
+ * @param {string} team Team to check
+ * @param {Game} game Game to check
+ * @returns `true` if team played in game, `false` otherwise.
+ */
+function played_in_game_p(team, game)
+{
+	const { home, away } = game;
+	return home.name === team || away.name === team;
+}
+
+
+/**
+ * Get the number of points a team received for a set of games.
+ * @param {string} team Team whose point total should be calculated
+ * @param {Game[]} games The games to calculate points from
+ */
+function points(team, games)
+{
+	return games.filter(game => played_in_game_p(team, game))
+				.map(game => points_for_game(team, game))
+				.reduce((p, acc) => p + acc, 0);
+}
+
+/**
+ * Get the status of the league based on a set of games.
+ * @param {string[]} teams The teams in the league
+ * @param {Game[]} games The games in the league
+ * @returns {TeamStatus[]} Array of teams and their points, ordered by
+ * position in the leauge
+ */
+export function league_status(teams, games)
+{
+	return teams.map(team => ({
+			name: team,
+			points: points(team, games)
+		})).sort((a, b) => b.points - a.points);
 }
--- src/lib/score.test.js
+++ src/lib/score.test.js
@@ -1,10 +1,99 @@
 import { describe, expect, it } from '@jest/globals';
-import { calculateStandings } from './score';
+import { league_status } from './score';
 
 describe('score', () => {
-  describe.only('calculateStandings', () => {
-    it('should have a test', () => {
-      expect(calculateStandings()).toBe(0);
-    });
-  });
+	describe('league_status', () => {
+		it('No games', () => {
+			expect(league_status(['lorem'], [])).toEqual([{
+				name: 'lorem',
+				points: 0
+			}]);
+		});
+
+		it('One game', () => {
+			expect(league_status(['lorem', 'ipsum'], [
+				{
+					home: {
+						name: 'lorem',
+						score: 3
+					},
+					away: {
+						name: 'ipsum',
+						score: 2
+					}
+				}
+			])).toEqual([
+				{
+					name: 'lorem',
+					points: 3
+				},
+				{
+					name: 'ipsum',
+					points: 0
+				}
+			]);
+		});
+
+		it('Draw', () => {
+			expect(league_status(['lorem', 'ipsum'], [
+				{
+					home: {
+						name: 'lorem',
+						score: 2
+					},
+					away: {
+						name: 'ipsum',
+						score: 2
+					}
+				}
+			])).toEqual([
+				{
+					name: 'lorem',
+					points: 1
+				},
+				{
+					name: 'ipsum',
+					points: 1
+				}
+			]);
+		});
+
+		it('Multiple games', () => {
+			expect(league_status(['lorem', 'ipsum', 'dolor'], [
+				{
+					home: {
+						name: 'lorem',
+						score: 2
+					},
+					away: {
+						name: 'ipsum',
+						score: 2
+					}
+				},
+				{
+					home: {
+						name: 'lorem',
+						score: 2
+					},
+					away: {
+						name: 'dolor',
+						score: 3
+					}
+				}
+			])).toEqual([
+				{
+					name: 'dolor',
+					points: 3
+				},
+				{
+					name: 'lorem',
+					points: 1
+				},
+				{
+					name: 'ipsum',
+					points: 1
+				}
+			]);
+		});
+	});
 });
